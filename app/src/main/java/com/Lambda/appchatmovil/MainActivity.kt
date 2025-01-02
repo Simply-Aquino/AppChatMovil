d00c24e5ae6e236ae6039e2ca114c3de4d09c094
04ngel25 <04ngel25@gmail.com>
2025-01-02 18:12:16 -0500

    Se colocó un xd al lado del nombre en el MainActivity xd

--- app/src/main/java/com/Lambda/appchatmovil/MainActivity.kt
+++ app/src/main/java/com/Lambda/appchatmovil/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 //Luis Corimanya
-//Angel Vellon
+//Angel Vellon xd
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

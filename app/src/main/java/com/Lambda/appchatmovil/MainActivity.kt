edf91beb52fed3929d8ed31aca10527129f88db8
Simply-Aquino <giancar2aquino@gmail.com>
2025-01-02 18:15:29 -0500

    Aquino Puso Nombre

--- app/src/main/java/com/Lambda/appchatmovil/MainActivity.kt
+++ app/src/main/java/com/Lambda/appchatmovil/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 //Luis Corimanya
-//Gian Aquino
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

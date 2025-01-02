0e06824ce9980df5cc2ca935ecc7d11ac3eea1ba
4P0L0-HLS <jacksernaque4@gmail.com>
2025-01-02 18:43:54 -0500

    JACK

--- app/src/main/java/com/Lambda/appchatmovil/MainActivity.kt
+++ app/src/main/java/com/Lambda/appchatmovil/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 //Luis Corimanya
 //Angel Vellon xd
 //Aquino Giancarlos :V
-//HOLA AMIGOS
+//HOLA Amigos
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

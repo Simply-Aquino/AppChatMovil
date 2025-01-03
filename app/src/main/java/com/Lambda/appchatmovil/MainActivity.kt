718120ac425c4bfae19147f0f034bc78b9b2c9cf
4nt0nioo <anthonysal7443@gmail.com>
2025-01-02 19:20:58 -0500

    Actualizar MainActivity.kt

--- app/src/main/java/com/Lambda/appchatmovil/MainActivity.kt
+++ app/src/main/java/com/Lambda/appchatmovil/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 //Angel Vellon xd
 //Aquino Giancarlos :V
 //HOLA Amigos
+// ooolaaaa
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

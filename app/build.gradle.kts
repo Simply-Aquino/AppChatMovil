2200666049b537e9f7fb673bc0ff4d75d1b5156d
Simply-Aquino <giancar2aquino@gmail.com>
2025-01-08 19:07:07 -0500

    Conexion con Firebase sin Storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,4 +47,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.airbnb.android:lottie:4.2.1")
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
 }
